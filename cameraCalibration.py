7d7b669e3dc8a32a75f5ed29ebdae48e371bf9bd
leonguenther <Leonsechteemailadresse@gmx.de>
2019-04-05 10:56:45 +0200

    clean up and comments

--- cameraCalibration.py
+++ cameraCalibration.py
@@ -7,15 +7,15 @@ import pickle
 
 
 
-	
-	
+
+
 def main():
 	testing = False
 
 	# Nao connection data
 	roboIP = '10.0.7.14'
 	port = 9559
-	
+
 	motionProxy = ALProxy("ALMotion",roboIP,port)
 	names = "HeadPitch"
 	angels = np.deg2rad(-10)
@@ -25,10 +25,13 @@ def main():
 	motionProxy.angleInterpolation(names,angels,times,isAbsolute)
 	motionProxy.setStiffnesses("Head", 0.0)
 
-	squarelen = 0.003
+	#length of the chessboard squares in meters
+	squarelen = 0.03
 	chessheigth = 9
 	chesslength = 6
 	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+	#arranging an array with the positions of the chessboard square cornerSubPix
+	#[(0, 0, 0), (0.03, 0, 0), (0.06, 0, 0) ...]
 	objp = np.zeros((chesslength * chessheigth,3), np.float32)
 	grid = np.mgrid[0:9 * squarelen:squarelen,0:chesslength * squarelen:squarelen].T.reshape(-1,2)
 	#no idea why mgrid does a (63,2) matrix here
@@ -36,71 +39,53 @@ def main():
 	# Arrays to store object points and image points from all the images.
 	objpoints = [] # 3d point in real world space
 	imgpoints = [] # 2d points in image plane.
-
-	# Connect with ALMotion
-	#motionProxy = ALProxy("ALMotion",roboIP, port)
-
-	# wake up
-	#motionProxy.wakeUp()
-
 	# Connect with ALVideoDevice
-	print()
 	visionProxy = ALProxy("ALVideoDevice",roboIP,port)
-
-
 	# Set up camera
-	resolution = vision_definitions.kVGA
-	colorSpace = 11
+	resolution = vision_definitions.kVGA # 640 x 480
+	colorSpace = 11 #RGB
 	fps = 1
-
 	# Subscribe camera
 	nameId = visionProxy.subscribeCamera("python_GVM",1,resolution, colorSpace, fps)
 
-
-	# Get image
-	#img = visionProxy.getImageRemote(nameId)
-
-	# convert image
-	#img = np.fromstring(img[6], dtype=np.uint8).reshape((480, 640, 3))
-
 	if testing:
-		#show image
-		#cv2.imshow('img', img)
-		#cv2.waitKey(10000)
 		pass
 	else:
-
-		while len(objpoints) < 100:
+		#we need at least 40 frames where we found the chessboard
+		while len(objpoints) < 40:
 			print("next Picture")
+			#get image from robot
 			img = visionProxy.getImageRemote(nameId)
-
+			#converting to opencv format
 			img = np.fromstring(img[6], dtype=np.uint8).reshape((480, 640, 3))
-
+			#grayscale image for easier detection
 			gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+			#find cheassboard
 			ret, corners = cv2.findChessboardCorners(gray, (chessheigth ,chesslength), None)
+			#if the chessboard is found add data to arrays
 			if ret == True:
 				objpoints.append(objp)
 				print(len(objpoints))
+				#increase accuracy of detected corners
 				corners2=cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-				imgpoints.append(corners)
+				imgpoints.append(corners2)
 
 				# Draw and display the corners
 				cv2.drawChessboardCorners(img, (chessheigth ,chesslength), corners2, ret)
 				cv2.imshow('img', img)
+			#if no chessboard is found still show the image
 			else:
 				cv2.imshow('img', img)
-
-			# elif ret == False:
-			# cam.release()
-			#advance frames with anykey, close with q
+			#end early by holidng Q
 			if cv2.waitKey(10) & 0xFF == ord('q'):
-				# cam.release()
 				break
+
 		cv2.destroyAllWindows()
 		print("out")
+		#use date to calibrate camera
 		ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-		print(mtx)
-		p = open('test.pckl', 'wb')
+		#save values to file
+		p = open('calRobo.pckl', 'wb')
 		pickle.dump((mtx, dist), p)
 		p.close()
 
--- project.py
+++ project.py
@@ -7,11 +7,11 @@ import vision_definitions
 import math
 import time
 
-#load cal
-p = open('CalRobo.pckl', 'rb')
+#Load calibration for pose estimation later
+p = open('calRobo.pckl', 'rb')
 cameraMatrix, distCoeffs = pickle.load(p)
 p.close()
-#Generate board
+#Generate marker or marker board
 para = aruco.DetectorParameters_create()
 dic = aruco.Dictionary_get(aruco.DICT_6X6_1000)
 board = aruco.GridBoard_create(
@@ -20,29 +20,28 @@ board = aruco.GridBoard_create(
 	markerLength=0.09,
 	markerSeparation=0.01,
 	dictionary=dic)
-#con settings
+#Connection settings
 roboIP = "10.0.7.14"
 port = 9559
-#vid settings
-resolution = vision_definitions.kVGA
-colorSpace = 11
+#Video settings
+resolution = vision_definitions.kVGA #640 x 480
+colorSpace = 11 #RGB colorspace
 fps = 1
-#proxys
+#Modul Proxys
 motionProxy = ALProxy("ALMotion",roboIP,port)
 visionProxy = ALProxy("ALVideoDevice",roboIP,port)
 postureProxy = ALProxy("ALRobotPosture",roboIP,port)
 visionProxy.setParameter(0, 22, 2)
-# subscribe camera
+#Subscribe to camera
 nameId = visionProxy.subscribeCamera("python_GVM",1,resolution, colorSpace, fps)
-
-turnD = 10
+#Turn value
+turnD = np.deg2rad(15)
 
 def main():
 	# wake up
 	motionProxy.setStiffnesses("Head", 1.0)
 	print("Standing up...")
 	motionProxy.wakeUp()
-	#motionProxy.setMoveArmsEnabled(True, True)
 	postureProxy.goToPosture("StandInit", 1.0)
 	print("Start search...")
 	search()
@@ -57,27 +56,23 @@ def main():
 def turnLeft():
 	x = 0.0
 	y = 0.0
-	theta = np.deg2rad(turnD)
+	theta = turnD
 	freq = 0.1
-	#motionProxy.moveInit()
 	motionProxy.moveTo(x,y,theta)
 
 def turnRight():
 	x = 0.0
 	y = 0.0
-	theta = np.deg2rad(-1 * turnD)
+	theta = -1 * turnD
 	freq = 0.1
-	#motionProxy.moveInit()
 	motionProxy.moveTo(x,y,theta)
 
 def startGoing():
-	x = 0.15
+	x = 0.15 # 15cm
 	y = 0
 	theta = 0
 	freq = 0.1
-	#motionProxy.moveInit()
 	motionProxy.moveTo(x,y,theta)
-
 
 def lookDown():
 	names = "HeadPitch"
@@ -96,13 +91,9 @@ def lookUp():
 def stopGoing():
 	motionProxy.stopMove()
 
-def startModul(modul):
-	return ALProxy(modul,roboIP,port)
-
 def look():
-	# get image
-	#cv2.waitKey(2000)
 	print("Start loading the image...")
+	#Get image from subcribedCamera
 	img = visionProxy.getImageRemote(nameId)
 	visionProxy.releaseImage(nameId)
 	if img == None :
@@ -110,19 +101,20 @@ def look():
 		visionProxy.unsubscribe(nameId)
 		motionProxy.rest()
 		exit()
-	# get the binary values of the image
+	#Converting to opencv format
 	img = np.fromstring(img[6], dtype=np.uint8).reshape((480, 640, 3))
 	print("Image loaded and converted...")
-	# detect chessboard --------------------------------
-	h, w, _ = img.shape
-	#remove colors
+
+	#converting to grayscale for easier marker detection
 	gImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	#draw crosshiar
+	h, w, _ = img.shape
 	img = cv2.line(img, (0, int(h/2)), (w, int(h/2)), (255, 0, 0))
 	img = cv2.line(img, (int(w/2), 0), (int(w/2), h), (255, 0, 0))
 	#detect markers
 	corners, ids, rejectedImgPoints = aruco.detectMarkers(gImg, dic, parameters = para)
-	#find missing markers with help of already found markers
+	#find missing markers with help of already found markers, only usefull if you use a board with mulitple markers
+	'''
 	corners, ids, rejectedImgPoints, recoveredIds = aruco.refineDetectedMarkers(
 		image = gImg,
 		board = board,
@@ -131,132 +123,112 @@ def look():
 		rejectedCorners = rejectedImgPoints,
 		cameraMatrix = cameraMatrix,
 		distCoeffs = distCoeffs)
-	#draw markers
+	'''
+	#draw green border around detected markers (for debugging)
 	img = aruco.drawDetectedMarkers(img, corners, borderColor=(0, 255, 0))
+	#estimate position of the marker
 	pose, rvec, tvec = aruco.estimatePoseBoard(corners, ids, board, cameraMatrix, distCoeffs)
- 
+	#if a positon is found
 	if pose:
-		#tvec[0] = tvec[0] - 0.03
-		#tvec[1] = tvec[1] - 0.03
+		#draw XYZ axis on marker (for debugging)
 		img = aruco.drawAxis(img, cameraMatrix, distCoeffs, rvec, tvec, 0.3)
-		#print(tvec)
+		#tvec[0] contains the postion of the marker on the x axis relative to camera middle point
+		#if tvec[0] is close to 0 the marker must be ahead
 		if abs(tvec[0]) < 0.05:
 			ret = "ahead"
+		#marker to the left
 		elif tvec[0] < 0:
 			ret = "left"
+		#marker to the rigth
 		elif tvec[0] > 0:
 			ret = "right"
+	#if no postion is detected we didnt find the marker
 	else:
 		ret = "notFound"
-
-	#img = cv2.imsize(img, (640,480))
+	#show the img (for debugging)
 	cv2.imshow('img', img)
-	return ret 
+	return ret
 
 def search():
-
 	i = 0
 	isLookDown = False
 	lookUp()
-	#find marker and positon robo staright
-	#should find the picture in 24 steps...24*15 = 360 ...
+	#Phase 1
+	#should find the picture in 24 steps...24*15° = 360° ...
 	while i < 25:
 		i= i+1
-		print "Searching the Picture: " , i
+		print("Searching the Marker: " , i)
 		dir = look()
-		print("Picture is " + dir)
+		print("Marker is " + dir)
+		#if marker is ahead start phase 2
 		if dir == "ahead":
 			#marker is ahead
 			startGoing()
 			break
-
+		#marker left
 		elif dir == "left":
 			turnLeft()
-
+		#marker left
 		elif dir == "right":
 			turnRight()
-
+		#if no marker is found, it migth be close, so we look at the ground
 		elif dir == "notFound" and isLookDown == False:
 			lookDown()
 			isLookDown = True
-
+		#if marker is not close we look up again and turn left
 		elif dir == "notFound" and isLookDown == True:
 			lookUp()
 			turnLeft()
 			isLookDown = False
-
+		#this shouldnt happen
 		else:
-			print("HOW")
-
+			print("Something odd just happened...")
+		#waiting 3 seconds(to not overtax the robot) or until a key is pressed. Holding down Q exits Phase 1
 		if cv2.waitKey(3000) & 0xFF == ord('q'):
 			stopGoing()
 			break
-
-	print("Found the Picture ....Going for it...")
+	#Phase 2
+	print("Marker is ahead ....Going for it...")
 	i = 0
+	#15 * 15cm = 225m this should be enough to get to the marker
 	while i < 15:
 		i= i+1
-		print "Trying to walk to the Picture: " , i
+		print("Trying to walk to the marker: " , i)
 		dir = look()
-		print("Picture is " + dir)
+		print("Marker is " + dir)
+		#if we cant see the marker it must be close
 		if dir == "notFound" and isLookDown == False:
 			lookDown()
 			isLookDown = True
+		#if we cant find the marker after looking down we must be on it
 		elif dir == "notFound" and isLookDown == True:
 			print("Lost the Picture again. Am I standing on it?")
-			#startGoing()
 			break
+
 		elif dir == "ahead":
 			startGoing()
+		#we migth have to readjust
 		elif dir == "right":
 			turnRight()
+		#we migth have to readjust
 		elif dir == "left":
 			turnLeft()
+		#this shouldnt happen
 		else:
 			print("Something odd just happened...")
-
+		#waiting 3 seconds(to not overtax the robot) or until a key is pressed. Holding down Q exits Phase 2
 		if cv2.waitKey(3000) & 0xFF == ord('q'):
 			stopGoing()
 			break
-	print "making sure im on it"
+
+	print("Making sure im on it")
+	#looking one more time to make sure we didnt stop early
 	dir = look()
+	#if the marker is found we start again
 	if dir !=  "notFound":
-		print "Im not standing on it"
+		print("Im not standing on it")
 		search()
-	print "I am surely standing on the picture now..."
-
-
-'''
-		if notFound:
-			if pose:
-				print("found")
-			elif markerAhead:
-				names = "HeadPitch"
-				angels = 20.0
-				times = 4.0
-				isAbsolute = True
-				motionProxy.post.angleInterpolation(names,angels,times,isAbsolute)
-				cv2.imshow('img', img)
-			else:
-				print("not found")
-				turnLeft(motionProxy)
-
-		if pose:
-			notFound = False
-			# move axis to middle
-			#tvec[0] = tvec[0] - 0.1
-			#tvec[1] = tvec[1] - 0.1
-
-		else:
-			cv2.imshow('img', img)
-
-	#advance frames with anykey, close with q
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		stopGoing(motionProxy)
-		break
-'''
-
-
+	print("I am surely standing on the picture now...")
 
 if __name__ == "__main__":
 	main()
